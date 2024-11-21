04ae5e2acb692c737ca60936c7705827e13e800a
ZhaoFuheng <fuheng_zhao@ucsb.edu>
2024-11-20 23:48:17 -0600

    finalizing the experiments

--- Prompts/prompt_templates.py
+++ Prompts/prompt_templates.py
@@ -3,7 +3,8 @@ Q1:{Q1}
 Q2:{Q2}
 /* And the following database schema: */
 {schema}
-/* Are Q1 and Q2 sematically equivalent? Write your final answer using format decision = "equivalent" or decision = "inequivalent" */
+Note: The SQL predicate is case-sensitive when comparing string values.
+/* Are Q1 and Q2 semantically equivalent? Write your final answer using format decision = "equivalent" or decision = "inequivalent" */
 /* Let's think step by step. */
 '''
 
@@ -13,12 +14,13 @@ Q1:{Q1}
 Q2:{Q2}
 /* And the following database schema: */
 {schema}
-/* Let's think step by step. Are Q1 and Q2 sematically equivalent?
-1. Try one example database and check the output table of Q1 and Q2.
-2. If the outputs are identical, can you modify the example database such that the outputs of Q1 and Q2 are not identical?
+/* Are Q1 and Q2 semantically equivalent?
+1. Try one example database and check the output table of Q1 and Q2. Database is case-sensitive when comparing string values.
+2. If the outputs are identical, adjust the database to see how output tables of Q1 and Q2 change.
 3. After evaluating whether there exists a database such Q1 and Q2 output different tables, write your answer using format decision = "equivalent" or decision = "inequivalent".
 */
-1.
+/* Let's think step by step. */
+1. Consider the following example database instance, which is string value case-sensitive, and execute Q1 and Q2.
 '''
 
 
@@ -28,10 +30,12 @@ Q2:{Q2}
 /* And the following database schema: */
 {schema}
 /* Let's think step by step. Are Q1 and Q2 pragmatic equivalent (with same intention)?
-1. Explain Q1 and Q2.
-2. Do Q1 and Q2 share aligned logic or Q1 and Q2 have significant differences? Write your answer using format decision = "equivalent" or decision = "inequivalent". Let's think step by step. 
+1. You can assume database contain no NULL values and is not case sensitive.
+2. Explain Q1 and Q2 and understand their objective high-levely.
+3. Do Q1 and Q2 share aligned logic or Q1 and Q2 have significant differences? Write your answer using format decision = "equivalent" or decision = "inequivalent". Let's think step by step. 
 */
-1.
+1. Database contain no NULL values and is not case sensitive.
+2. 
 '''
 
 
@@ -39,42 +43,41 @@ semantic_static_prefix = '''/* some examples are provided */
 /* Given the following two SQL examples */
 example sql1: select distinct T1.id from customer AS T1, customer AS T2 where T1.id = T2.id and T1.order_id = 1
 example sql2: select distinct T1.id from customer AS T1, customer AS T2, customer AS T3 where T1.id = T2.id and T2.id = T3.id and T1.order_id = 1
-/* Are they sematically equivalent? */
+/* Are they semantically equivalent? */
 decision = "equivalent"
 
 /* Given the following two SQL examples */
 example sql1: select T1.avg_sale from singer AS T1
 example sql2: select AVG(T1.sale) from singer AS T1
-/* Are they sematically equivalent? */
+/* Are they semantically equivalent? */
 decision = "inequivalent"
 
 /* Given the following two SQL examples */
 example sql1: SELECT * FROM emp WHERE sal = 3 AND comm = sal + 5
 example sql2: SELECT * FROM emp WHERE sal = 3 AND comm = 8
-/* Are they sematically equivalent? */
+/* Are they semantically equivalent? */
 decision = "equivalent"
 
 /* Given the following two SQL examples */
 example sql1: select dept.name from dept where dept.id = 1
 example sql2: select dept.name from dept where dept.id = 10
-/* Are they sematically equivalent? */
+/* Are they semantically equivalent? */
 decision = "inequivalent"
 
 /* Given the following two SQL examples */
 example sql1: SELECT dept.deptno FROM dept AS dept GROUP BY dept.deptno, dept.deptno
 example sql2: SELECT dept.deptno FROM dept AS dept GROUP BY dept.deptno
-/* Are they sematically equivalent? */
+/* Are they semantically equivalent? */
 decision = "equivalent"
 '''
 
 
 relax_static_prefix = '''/* some examples are provided */
 /* Given the following two SQL examples */
-example sql1: SELECT DISTINCT releaseType FROM torrents where groupName != 'Christmas'
-example sql2: SELECT releaseType FROM torrents where groupName <> 'christmas'
+example sql1: SELECT COUNT(DISTINCT Country) FROM countries
+example sql2: SELECT COUNT(*) FROM countries
 /* Do they have the same intention or share aligned logic? */
-The only differeences are distinct and Christmas v.s. christmas.
-Because distinct and case sensitivity do not change the logic, they have no significant difference and they convey the same meaning.
+They are both determining how many rows in the countries table
 decision = "equivalent"
 
 /* Given the following two SQL examples */
